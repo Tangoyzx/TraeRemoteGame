3a467e4a1165dd4c0fb55b5c137221571584ca8f
Tangoyzx <Tangoyzx@gmail.com>
2026-07-10 14:26:57 +0800

    Replace text label with color box interaction

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,23 +1,23 @@
 extends Control
 
-const WELCOME_TEXT := "欢迎"
-const CLICKED_TEXT := "点击"
+const IDLE_COLOR := Color(1.0, 0.0, 0.0, 1.0)
+const CLICKED_COLOR := Color(0.0, 1.0, 0.0, 1.0)
 const CLICK_DURATION_SECONDS := 2.0
 
-@onready var message_label: Label = $Center/VBox/MessageLabel
+@onready var color_box: ColorRect = $Center/VBox/ColorBox
 @onready var click_button: Button = $Center/VBox/ClickButton
 
 var _click_generation := 0
 
 func _ready() -> void:
-	message_label.text = WELCOME_TEXT
+	color_box.color = IDLE_COLOR
 	click_button.pressed.connect(_on_click_button_pressed)
 
 
 func _on_click_button_pressed() -> void:
 	_click_generation += 1
 	var generation := _click_generation
-	message_label.text = CLICKED_TEXT
+	color_box.color = CLICKED_COLOR
 	await get_tree().create_timer(CLICK_DURATION_SECONDS).timeout
 	if generation == _click_generation:
-		message_label.text = WELCOME_TEXT
+		color_box.color = IDLE_COLOR
